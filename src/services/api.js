8068949a3f2f2ca712695c1d71525b1ecd07ac65
Juliancio Carvalho <juliancio.carvalho@fcamara.com.br>
2020-12-28 19:55:28 -0300

    fix api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `https://economia.awesomeapi.com.br/USD`,
+  baseURL: `http://economia.awesomeapi.com.br/USD`,
 });
 
 export default api;
